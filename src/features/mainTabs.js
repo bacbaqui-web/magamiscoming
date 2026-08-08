5939153bf6bfde704c11fcc582ab3365d15433a4
bacbaqui-web <cockrott@gmail.com>
2026-08-08 13:33:07 +0900

    fix: 상단 탭과 커스텀 화면 고정

--- src/features/mainTabs.js
+++ src/features/mainTabs.js
@@ -105,6 +105,7 @@ export function initMainTabs() {
   }
 
   function showCustomTab(tab, button) {
+    document.body.classList.add('custom-tab-view-active');
     document
       .querySelectorAll('.tab-content')
       .forEach((content) => content.classList.remove('active'));
